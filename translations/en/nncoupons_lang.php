9a6229efc447df86979675cd902121c8a6e24e75
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-15 15:51:31 +0530

    Add files via upload

---
+++ translations/en/nncoupons_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$sLangName = "English";
+
+$aLang = [
+    'charset'                           => 'UTF-8',
+    'COUPONSAVALIABLE'                  => 'Avaliable Coupons',
+    'AVALIABLE_COUPON_TITLE'            => 'Avaliable Coupons',
+    'MY_COUPON_LIST'                    => 'Display Avaliable Coupons',
+    'COUPON_ERROR_DISPLAY_NOVALNET'     => 'Coupons are Not Avaliable!',
+];
