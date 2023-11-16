bc576d517d0b2cfee8044f0f3e4b9daf8f8e51ff
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-16 12:01:49 +0530

    Add files via upload

---
+++ Controller/AvaliableCouponsController.php
@@ -0,0 +1,52 @@
+<?php
+namespace Nncoupons\Controller;
+use OxidEsales\Eshop\Core\Registry;
+use oxRegistry;
+use Nncoupons\Model\AvaliableCouponsModel;
+
+class AvaliableCouponsController extends \OxidEsales\Eshop\Application\Controller\FrontendController
+
+{
+ 
+    protected $_sThisTemplate = "@nncoupons/templates/displaycouponlist";
+
+    public function render()
+    {
+        parent::render();
+        $oUser = $this->getUser();
+        if (!$oUser) {
+            return $this->_sThisTemplate = $this->_sThisLoginTemplate;
+        }
+        
+        $voucherModel = oxNew(AvaliableCouponsModel::class);
+        $voucherSeriesData = $voucherModel->getVoucherSeriesData();
+        
+        $this->_aViewData["couponarray"] = $voucherSeriesData;
+
+        return $this->_sThisTemplate;
+    }
+    public function getBreadCrumb()
+    {
+        $aPaths = [];
+        $aPath = [];
+
+        $oLang = \OxidEsales\Eshop\Core\Registry::getLang();
+        $sSelfLink = $this->getViewConfig()->getSelfLink();
+
+        $iBaseLanguage = $oLang->getBaseLanguage();
+        $aPath['title'] = $oLang->translateString('MY_ACCOUNT', $iBaseLanguage, false);
+        $aPath['link'] = \OxidEsales\Eshop\Core\Registry::getSeoEncoder()->getStaticUrl($sSelfLink . "cl=account");
+        $aPaths[] = $aPath;
+
+        $aPath['title'] = $oLang->translateString('AVALIABLE_COUPON_TITLE', null, false);
+        $url = $this->getLink();
+        $url = htmlspecialchars_decode($url);
+        $aPath['link'] =$url;
+        $aPaths[] = $aPath;
+
+        return $aPaths;
+    }
+
+
+
+}
---
+++ metadata.php
@@ -0,0 +1,38 @@
+<?php
+
+$sMetadataVersion = '2.1';
+
+
+$aModule = [
+        'id'          => 'nncoupons',
+        'title'       => [
+            'de' => 'Novalnet Coupons',
+            'en' => 'Novalnet Coupons',
+        ],
+        'description' => [ 'de' => 'This Extension Helpfull for Display the Avaliable Coupons in USER My Account',
+                           'en' => 'This Extension Helpfull for Display the Avaliable Coupons in USER My Account',
+        ],
+        'thumbnail'   => 'img/avaliablecoupons.png',
+        'version'     => '2.1.0',
+        'author'      => 'Novalnet Developer',
+        'url'         => 'https://www.novalnet.com',
+        'email'       => 'technical@novalnet.de',
+        'extend'      => [
+            \OxidEsales\Eshop\Application\Controller\FrontendController::class => Nncoupons\Controller\AvaliableCouponsController::class,
+            
+        ],
+        'controllers'  => [  
+                'account_avaliablecoupons'         => Nncoupons\Controller\AvaliableCouponsController::class,
+            
+        ],
+
+
+        'settings'      => [
+            ['group' => 'couponsdisplay', 'name' => 'couponname','type' => 'str',   'value'  => '', 'position' => 1 ],
+            ['group' => 'couponsdisplay', 'name' => 'couponcode',    'type' => 'str',   'value'  => '', 'position' => 2 ],
+            
+        ],
+        'events'    => [
+           
+        ],
+];
