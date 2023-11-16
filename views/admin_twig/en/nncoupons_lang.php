77ac5c88a38fac50ff0bb562493c3bb064c3318f
krishtamilzhaa133 <134515146+krishtamilzhaa133@users.noreply.github.com>
2023-11-16 12:41:59 +0530

    Add files via upload

---
+++ views/admin_twig/en/nncoupons_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+$sLangName = 'English';
+
+$aLang = array(
+
+    'charset' => 'UTF-8',
+    'SHOP_MODULE_GROUP_couponsdisplay' => 'Coupon Setttings',
+    'SHOP_MODULE_couponname' => 'Coupon Name',
+    'SHOP_MODULE_couponcode' => 'Coupon Code',
+    
+
+);
